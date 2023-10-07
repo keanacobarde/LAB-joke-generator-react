89d717c464c5b67b32bd9140e075d32dfdb6dd86
Keana Cobarde <keanacobarde@gmail.com>
2023-10-07 11:58:27 -0500

    getting functionality to work - used incorrect evokation and missed a capital letter in parameter, line 8, Joke.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
+import { useState } from 'react';
+import Joke from '../components/Joke';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  const [joke, setJoke] = useState({});
+  const [btnText, setBtnText] = useState('Get A Joke');
+
+  const getAJoke = () => {
+    getJoke().then((obj) => setJoke({
+      setup: obj.setup,
+      delivery: obj.delivery,
+    }));
+    setBtnText('Get a Punchline');
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -9,7 +24,9 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Welcome Home!</h1>
+      <h1>Get a Joke!</h1>
+      <Joke joker={joke} btnText={btnText} />
+      {btnText === 'Get A Joke' || btnText === 'Get A New Joke' ? (<button type="button" onClick={getAJoke}> {btnText} </button>) : (<button type="button" onClick={() => setBtnText('Get A New Joke')}> {btnText} </button>) }
     </div>
   );
 }
